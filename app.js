e75a7663d121a975bcb1a17ce35270aec06c5beb
spokzers <spokzers@gmail.com>
2018-03-29 11:28:55 +0500

    updatessss

--- app.js
+++ app.js
@@ -139,9 +139,8 @@ app.post('/account/profile', passportConfig.isAuthenticated, userController.post
 app.post('/account/password', passportConfig.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConfig.isAuthenticated, userController.postDeleteAccount);
 app.get('/account/unlink/:provider', passportConfig.isAuthenticated, userController.getOauthUnlink);
-app.get('/issues/create', passportConfig.isAuthenticated, issueController.creation);
-app.get('/issues', passportConfig.isAuthenticated, issueController.index)
 
+app.get('/issues', passportConfig.isAuthenticated, issueController.index)
 app.get('/issues/create', passportConfig.isAuthenticated, issueController.getCreate);
 app.post('/issues/create', passportConfig.isAuthenticated, issueController.postCreate);
 app.post('/issues/:issue_no/comment', passportConfig.isAuthenticated, issueController.postComment);
--- controllers/issue.js
+++ controllers/issue.js
@@ -76,28 +76,12 @@ exports.postCreate = (req, res) => {
 
 
 exports.index = (req, res) => {
-	res.render('issues/index', {
-		title: 'Issue Listing',
-		issues: [{
-			title: "your disc",
-			posted_by: "spokzers",
-			posted_on: "2018-03-28 20:34",
-			tags: ["container", "asdasdasd"]
-		},{
-			title: "your disc",
-			posted_by: "spokzers",
-			posted_on: "2018-03-28 20:34",
-			tags: ["container", "asdasdasd"]
-		},{
-			title: "your disc",
-			posted_by: "spokzers",
-			posted_on: "2018-03-28 20:34",
-			tags: ["container", "asdasdasd"]
-		},{
-			title: "your disc",
-			posted_by: "spokzers",
-			posted_on: "2018-03-28 20:34",
-			tags: ["container", "asdasdasd"]
-		}]
+  Issue
+    .find({})
+    .then(issues => {
+      res.render('issues/index', {
+    		title: 'Issue Listing',
+    		issues: issues
+    })
 	});
 };
